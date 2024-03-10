5d95b4808e4bd08a160cfbd14738509ca4659b55
jaxridd <jayriddell04@gmail.com>
2024-03-10 00:23:58 -0600

    Initial commit

---
+++ app/search/[term]/page.tsx
@@ -0,0 +1,19 @@
+import { notFound } from "next/navigation";
+
+type Props = {
+  params: {
+    term: string;
+  };
+};
+
+function SearchPage({ params: { term } }: Props) {
+  if (!term) notFound();
+
+  const termToUse = decodeURI(term);
+
+  // API call to get the Searched Movies
+  // API call to get the Popular Moveis
+  return <div>Welcome to the search page: {termToUse}</div>;
+}
+
+export default SearchPage;
